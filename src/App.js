6b94dd7ad4d944ad32d72c23785f0ff3137338a3
bookpark <bkbkgg@gmail.com>
2022-12-13 10:09:13 +0900

    redux 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import Header from "./Header";
+
+function reducer(currentState, action) {
+    if (currentState === undefined) {
+        return ({
+            Authorization: '',
+            UserId: ''
+        })
+    }
+    const newState = {...currentState};
+    switch (action.type) {
+        case "NEWTOKEN":
+            newState.Authorization = action.data;
+            break;
+        case "USERID":
+            newState.UserId = action.data;
+    }
+    return newState;
+}
+
+const store = createStore(reducer)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Header/>
+                    <Routes>
+                        <Route exact path="/" element={<Login/>}/>
+                        <Route exact path="/" element={<User/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
